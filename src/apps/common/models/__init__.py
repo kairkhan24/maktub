182193db41492cb4d9ddaf3d604c10b11fdbe3af
rakhatuly <999.9plus0.1@gmail.com>
2021-12-28 19:56:51 +0600

    user model inherits common models

---
+++ src/apps/common/models/__init__.py
@@ -0,0 +1,12 @@
+from apps.common.models.uuid import UUIDModel
+from apps.common.models.timestamp import TimeStampModel
+from apps.common.models.personal_data import PersonalDataModel
+from apps.common.models.not_deletable import NotDeletableModel
+
+
+__all__ = (
+    'UUIDModel',
+    'TimeStampModel',
+    'PersonalDataModel',
+    'NotDeletableModel',
+)
--- src/apps/users/models/user.py
+++ src/apps/users/models/user.py
@@ -1,9 +1,22 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from apps.common.models import (
+    UUIDModel,
+    TimeStampModel,
+    NotDeletableModel,
+    PersonalDataModel
+)
 
 
-class User(AbstractUser):
+class User(UUIDModel,
+           TimeStampModel,
+           NotDeletableModel,
+           PersonalDataModel,
+           AbstractUser,
+           ):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
+    # objects = UserManager()
